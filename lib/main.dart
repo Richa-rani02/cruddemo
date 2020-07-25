059674b835c549430e3ebac4dc96546fe6383947
Richa <richa.rani1997@gmail.com>
2020-07-25 13:33:41 +0530

    Screenshots Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,30 +1,23 @@
-
 import 'package:cruddemo/CrudApp/create.dart';
 import 'package:flutter/material.dart';
 
 import 'CrudApp/crud_sample.dart';
 import 'login.dart';
 
-
 void main() => runApp(new MyApp());
 
 class MyApp extends StatelessWidget {
-
-
-
   @override
   Widget build(BuildContext context) {
     return new MaterialApp(
       debugShowCheckedModeBanner: false,
       title: 'Flutter Demo',
       theme: new ThemeData(
-        primarySwatch: Colors.blue,
+          primarySwatch: Colors.blue,
           accentColor: Colors.black,
           inputDecorationTheme: const InputDecorationTheme(
-
             hintStyle: TextStyle(color: Colors.black),
           )),
-
 
       // home: new WallScreen(analytics: analytics, observer: observer),
       home: new Add(),
